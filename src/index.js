df2f7a692ddf9afa2556de1f3dcfc89147bd214e
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2024-10-06 21:18:57 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.getElementById("create-task-form");
+  const taskInput = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const newTaskText = taskInput.value;
+
+    if (newTaskText === "") {
+      alert("Task description cannot be empty!");
+      return;
+    }
+
+
+    const newTaskItem = document.createElement("li");
+
+
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "X";
+
+
+    newTaskItem.textContent = newTaskText + " ";
+    newTaskItem.appendChild(deleteButton);
+
+
+    taskList.appendChild(newTaskItem);
+
+
+    taskInput.value = "";
+
+    deleteButton.addEventListener("click", function () {
+      taskList.removeChild(newTaskItem);
+    });
+  });
 });
+
